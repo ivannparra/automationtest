99fc184a06d67bd1d098a4f725b57df0bbc4d272
Ivan Parra <ivan@thecloseapp.com>
2021-09-09 16:09:21 +0200

    new changes

--- cypress/integration/Tests/TestExample.js
+++ cypress/integration/Tests/TestExample.js
@@ -1,6 +1,12 @@
+/// <reference types="Cypress" />
+
 describe("ExampleTestSuite", () => {
 it("testOne", () => {
-cy.visit("https://saucedemo.com/")
+cy.visit('https://saucedemo.com/')
+cy.get('#user-name').type('standard_user')
+cy.get('#password').type('secret_sauce')
+cy.get('#login-button').click()
+cy.url().should('eq', 'https://www.saucedemo.com/inventory.html')
 })  
 //it("testTwo", () =>{
 //
